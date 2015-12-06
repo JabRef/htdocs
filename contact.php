d0d3f364eae8e3fcf6cc22c57fb5e3c5a933c011
Oliver Kopp <kopp.dev@gmail.com>
2015-12-06 13:48:56 +0100

    Add references to new pages instead of serving old content

--- contact.php
+++ contact.php
@@ -13,59 +13,8 @@
 
     <?php include("navigation.php"); ?>
 
-    <div id="rightpanel">
-
-      <h3>Contact</h3>
-
-      <span class="single_item"><a href="#mailinglist">Mailing lists</a></span>
-      <span class="single_item"><a href="#git">Code repository</a></span>
-      <span class="single_item"><a href="#bugtracker">Bug tracker</a></span>
-      <span class="single_item"><a href="#featurerequest">Feature request</a></span>
-      <span class="single_item"><a href="#patches">Patches</a></span>
-      <span class="single_item"><a href="#donations">Donations</a></span>
-
-    </div>
-
-
     <div id="main">
-      <h2>Contact</h2>
-
-      <p>As a user, you can contribute to <acronym>JabRef's</acronym> development by
-      sharing your wishes for new features, comments or information
-      about bugs you have found. There are several ways to interact
-      with us, depending on what you want to achieve.</p>
-
-      <h3 id="mailinglist">Mailing list</h3>
-      <p>For general questions about <acronym>JabRef</acronym>, you can use the
-      <a href="https://lists.sourceforge.net/lists/listinfo/jabref-users"><acronym>JabRef</acronym> user mailing list</a>.
-      An overview on all available mailinglists is provided at our <a href="http://sourceforge.net/p/jabref/mailman/">mailing list page</a>.</p>
-
-      <h3 id="git">Repository access</h3>
-      <p>To browse or clone the <acronym>JabRef</acronym> source code from Git,
-      take a look at our
-      <a href="https://github.com/JabRef/jabref">Github page</a>.</p>
-
-      <h3 id="bugtracker">Bug reports</h3>
-      <p>To register bug reports, go to the
-      <a href="https://github.com/JabRef/jabref/issues">Bug tracker</a>.</p>
-
-      <h3 id="featurerequest">Feature request</h3>
-      <p>To register feature requests, go to the
-      <a href="https://github.com/JabRef/jabref/issues">Feature request page</a>.</p>
-
-      <h3 id="patches">Patches</h3>
-      <p>We migrated our source code to <a href="https://github.com/JabRef/jabref">Github</a>. 
-      If you want to place a code contribution (improvement or bugfix) for <acronym>JabRef</acronym>, 
-      please send a pull request to our <a href="https://github.com/JabRef/jabref">github repo</a>
-      for possible inclusion in future versions of <acronym>JabRef</acronym>.
-      If you want, also let us know on the <acronym>JabRef</acronym> mailing list about your new code.</p>
-
-      <h3 id="donations">Donations</h3>
-      <p>Finally, if you enjoy using <acronym>JabRef</acronym> and would like to make
-      a donation, take a look at our
-      <a href="http://sourceforge.net/p/jabref/donate/">
-      Donations page</a>.</p>
-
+      <p>Please go to <a href="http://www.jabref.org/#support">www.jabref.org/#support</a>.</p>
     </div>
     <?php include("footer.php"); ?>
   </div>
--- download.php
+++ download.php
@@ -14,27 +14,7 @@
     <?php include("navigation.php"); ?>
 
     <div id="main">
-
-    <p>
-    We use <a href="https://www.ej-technologies.com/products/install4j/overview.html">install4j</a>, the multi-platform installer builder.</p>
-
-    <h2>Download</h2>
-
-    <p>Latest releases are always available at <a href="https://github.com/JabRef/jabref/releases">https://github.com/JabRef/jabref/releases</a>.</p>
-
-    <h3 id="stable">Latest stable version</h3>
-    <p>We recommend the stable version for all casual users. These releases are beta-tested and expected to be stable and reliable.</p>
-    <p>
-      <a href="http://sourceforge.net/projects/jabref/files/v3.0"><img src="images/Download-stable.png" alt="Download current stable version" /></a>
-    </p>
-
-    <h3 id="dev">Developer versions</h3>
-    <p>
-      To browse or check out the <acronym>JabRef</acronym> developer source code from Git, take a look at our
-      <a href="https://github.com/JabRef/jabref">Github page</a>.
-      Latest builds are made available at <a href="http://builds.jabref.org/master/">http://builds.jabref.org/master/</a>.
-    </p>
-
+      <p>Please go to <a href="http://www.jabref.org/#downloads">www.jabref.org/#downloads</a>.</p>
     </div>
     <?php include("footer.php"); ?>
   </div>

c718b5e9c05a2a3221ccb42930814ff028817342
Oliver Kopp <kopp.dev@gmail.com>
2016-04-17 23:29:40 +0200

    Implement redirect using HTTP status code 301

--- faq.php
+++ faq.php
@@ -1,15 +1,1 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=http://www.jabref.org/faq/">
-        <script type="text/javascript">
-            window.location.href = "http://www.jabref.org/faq/"
-        </script>
-        <title>Page Redirection</title>
-    </head>
-    <body>
-        The JabRef FAQ has been reworked.
-        If you are not redirected automatically, follow the <a href='http://www.jabref.org/faq'>link to the new FAQ</a>
-    </body>
-</html>
+<?php header("Location: http://www.jabref.org/faq", true, 301); ?>

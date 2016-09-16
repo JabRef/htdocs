7bd1b416b4a08bf6699976910317693c5a7bc602
Oliver Kopp <kopp.dev@gmail.com>
2016-09-16 09:32:29 +0200

    Add redirects to de help files

--- help/de/RevisionHistory.php
+++ help/de/RevisionHistory.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
 <?php
-  header('Content-type: application/xhtml+xml; charset=utf-8');
-
-  // DO NOT EDIT BY HAND
-  // This file is generated from jabref/src/help.
-  // Run generate-htdocs-help-from-jabref-src-help.pl in the root directory
-  // of the JabRef repository to regenerate the htdocs out of JabRef's help.
+  header("Location: https://help.jabref.org/de/RevisionHistory", true, 301);
+  die();
 ?>
-<head>
-  <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-  <title>Versionsgeschichte (auf Englisch)</title>
-  <link href="/css/style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-  <div id="container">
-    <?php include("../../navigation.php"); ?>
-    <a href="Contents.php">Zur&uuml;ck zum Inhaltsverzeichnis</a>
-
-
-  <h1>Versionsgeschichte (auf Englisch)</h1>
-
-    <p>Die vollst&auml; Versionsgeschichte ist auf Englisch unter <a href=http://jabref.sourceforge.net/revisionhistory.php">http://jabref.sourceforge.net/revisionhistory.php</a> zu finden.</p>
-
-  <?php include("../../footer.php"); ?>
-  </div>
-
-</body>
-</html>

e7d114e94a356ea98dec0ce9c07abe268cb3ac4e
Oliver Kopp <kopp.dev@gmail.com>
2017-02-15 13:24:33 +0100

    Add redirect at translation

--- translation.php
+++ translation.php
@@ -1,43 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-  <title>JabRef reference manager - Translating JabRef</title>
-  <link href='css/style.css' rel='stylesheet' type='text/css' />
-</head>
-
-<body>
-  <div id="container">
-
-    <?php include("navigation.php"); ?>
-
-    <div id="rightpanel">
-
-      <h3>Translating JabRef</h3>
-
-      <div class="contributors">
-        <h4 id="translators">Translators</h4>
-          <span class="developer">Morten O. Alver <img src="images/stock_person.png" alt="translator" title="Norwegian (no) translator" /></span>
-          <span class="developer">Thomas Arildsen <img src="images/stock_person.png" alt="translator" title="Danish (da) translator" /></span>
-          <span class="developer">Fr&eacute;d&eacute;ric Darboux <img src="images/stock_person.png" alt="translator" title="French (fr) translator" /></span>
-          <span class="developer">Ambrogio Oliva <img src="images/stock_person.png" alt="translator" title="Italian (it) translator" /></span>
-          <span class="developer">Gert Renckens <img src="images/stock_person.png" alt="translator" title="Dutch (nl) translator" /></span>
-          <span class="developer">Igor Steinmacher<img src="images/stock_person.png" alt="translator" title="Portuguese (pt-BR) translator" /></span>
-          <span class="developer">Jorge Tornero <img src="images/stock_person.png" alt="translator" title="Spanish (es) translator" /></span>
-          <span class="developer">Aradenatorix Veckhom <img src="images/stock_person.png" alt="translator" title="Spanish (es) translator" /></span>
-          <span class="developer">Dominik Wa&szlig;enhoven <img src="images/stock_person.png" alt="translator" title="German (de) translator" /></span>
-          <span class="developer">Wenbo Yang <img src="images/stock_person.png" alt="translator" title="Simplified Chinese (zh) translator" /></span>
-          <span class="developer">Koji Yokota <img src="images/stock_person.png" alt="translator" title="Japanese (ja) translator" /></span>
-      </div>
-    </div>
-
-    <div id="main">
-      <p>The content of this page moved to <a href="https://github.com/JabRef/jabref/wiki/Translating-JabRef">JabRef's Wiki at github</a>.</p>
-    </div>
-    <?php include("footer.php"); ?>
-  </div>
-
-</body>
-</html>
+<?php
+  header("Location: http://help.jabref.org/en/TranslatingGUI", true, 301);
+  die();
+?>
